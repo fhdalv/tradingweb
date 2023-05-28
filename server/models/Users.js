47d0f1668eb757786005cae266d75bee62c70b8d
MKHLink <likhonhasankomol@gmail.com>
2023-05-28 17:30:03 -0400

    Added bcrypt and added methods to hash database password and metod to check is password is correct

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,4 +1,5 @@
 const {Schema, model} = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const usersSchema = new Schema(
     {
@@ -24,9 +25,26 @@ const usersSchema = new Schema(
         }
     },
     {
+        toJSON:{
+            virtuals: true
+        },
+
         id: false
     }
 );
+
+//password hasher
+usersSchema.pre('save',async function(next){
+    if(this.isNew||this.isModified('password')){
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password,saltRounds);
+    }
+    next();
+});
+
+usersSchema.methods.isCorrectPassword = async function(pass){
+    return bcrypt.compare(pass, this.password);
+}
 
 const Users = model('Users', usersSchema);
 
